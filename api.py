c7b68d33ed86653ad5f5068ce5f3408ba54bcd47
youngtakcho <46730544+youngtakcho@users.noreply.github.com>
2019-05-11 02:17:54 -0500

    Add files via upload

--- api.py
+++ api.py
@@ -2,18 +2,36 @@ import sqlite3
 import gensim
 import pickle
 from gensim.parsing import *
-from gensim.similarities.docsim import Similarity
+# from gensim.similarities.docsim import Similarity
 from flask import Flask , render_template , request , url_for
-from flask_restful import Resource, Api
 from flask_table import Table, Col , LinkCol
+from sklearn.externals import joblib
 import copy
 import time
-app = Flask(__name__)
+from TextModule import *
 
 
 def preprocess(text):
     CUSTOM_FILTERS = [lambda x: x.lower(), strip_tags, strip_punctuation,stem_text,remove_stopwords,strip_multiple_whitespaces,strip_short]
     return gensim.parsing.preprocess_string(text,CUSTOM_FILTERS)
+
+app = Flask(__name__)
+
+# app.dictionary = gensim.corpora.Dictionary.load("./dictionary.gensim")
+
+# app.data = None
+print("load libs")
+
+
+# app.count_vect = joblib.load("MyCountVectorizer.pickle")
+# app.tfidf_transformer = joblib.load("MyTfIdfVectorizer.pickle")
+# app.clf = joblib.load("LinearSVC.pickle")
+
+# app.clf = joblib.load("MyNaiveBayes.pickle")
+app.tfidf_use = False
+print("load dic")
+# with open('dictionary.pickle', 'rb') as handle:
+#     app.data = pickle.load(handle)
 
 
 @app.route('/')
@@ -30,21 +48,26 @@ def search():
         query = request.args.get('query')
     elif request.method == "POST":
         query = request.form["query"]
+    docs_new = [query]
+    X_new_counts = app.count_vect.transform(docs_new)
+
+    X_new_tfidf = app.tfidf_transformer.transform(X_new_counts)
+
+    predicted = app.clf.predict(X_new_counts)[0]
+
     query_arr = preprocess(query)
-    dictionary = copy.deepcopy(app.dictionary)
-    dictionary.add_documents([query_arr])
-    bow_q = dictionary.doc2bow(query_arr)
     results = set()
 
     try:
-        results = app.data[query_arr[0]]
+        results = app.count_vect.inverted_index_dict[query_arr[0]]
     except KeyError as e:
         print("no key")
     for i in query_arr[1:]:
         try:
-            results = results.intersection(app.data[i])
+            results = results.intersection(app.count_vect.inverted_index_dict[i])
         except KeyError as e:
             continue
+
 
     conn = sqlite3.connect("game_data_modi.db")
     c = conn.cursor()
@@ -55,34 +78,37 @@ def search():
     bows = []
     indices = []
     for idx, review in rows:
-        pre = preprocess(review)
-        bow = dictionary.doc2bow(pre)
-        bows.append(bow)
+        # pre = preprocess(review)
+
+        bows.append(review)
         indices.append(idx)
+    bows = app.count_vect.transform(bows)
+    bows = app.tfidf_transformer.transform(bows)
+    siml = Similarity()
+    result_siml = siml.calculate(bows,X_new_tfidf)
+    # siml = Similarity(None, bows, num_features=len(dictionary))
+    # result_siml = siml[bow_q]
 
-    siml = Similarity(None, bows, num_features=len(dictionary))
-    result_siml = siml[bow_q]
-
-    ordered = sorted(range(len(result_siml)), key=lambda k: result_siml[k])
+    ordered = sorted(range(len(result_siml)), key=lambda k: result_siml[k],reverse=True)
     r_list = []
     for i in ordered:
         r_list.append(indices[i])  # list of reviews.
+    pre_ordered = sorted(range(len(predicted)), key=lambda k: predicted[k][0],reverse=True)
+    pre_list = []
+    for i in pre_ordered:
+        pre_list.append(predicted[i])  # list of reviews.
 
-    # sql_query = "select product_id from reviews where r_id IN "
-    # sql_query += str(tuple(r_list))
-    # print(sql_query)
+
     conn = sqlite3.connect("game_data.db")
     c = conn.cursor()
-    # c.execute(sql_query)
-    # rows = c.fetchall()
-    sql_query = "select product_id from reviews where r_id = "
+
+    # sql_query = "select product_id from reviews where r_id = "
     d_pr = {}
     product_set = set()
     for i in r_list:
         c.execute("select product_id from reviews where r_id = " + str(i))
         rows = c.fetchall()
         for r in rows:
-            print(r)
             if r[0] is None or type(r[0]) == str:
                 print("None")
                 continue
@@ -92,8 +118,21 @@ def search():
                 d_pr[r[0]].append(i)
             product_set.add(r[0])
     t_rows = []
+    classified_set = set()
     for pro in product_set:
-        print("select title,developer,genres,tags from products WHERE id=" + str(pro))
+        q = "select title,developer,genres,tags from products WHERE id=" + str(pro) +""" and genres like "%"""+str(pre_list[0][1])+"""%\""""
+        print(q)
+        c.execute(q)
+        rows = c.fetchall()
+        for title, developer, tags, genres in rows:
+            classified_set.add(pro)
+            link_str = ""
+            for r_id in d_pr[pro][:5]:
+                link_str += """<a href=\"/review?r_id="""+str(r_id)+"""\" target=\"_blank\">"""+str(r_id)+""",</a>"""
+            t_rows.append(Item(title, developer, tags, genres,link_str))
+    product_set = product_set-classified_set
+
+    for pro in product_set:
         c.execute("select title,developer,genres,tags from products WHERE id=" + str(pro))
         rows = c.fetchall()
         for title, developer, tags, genres in rows:
@@ -101,6 +140,7 @@ def search():
             for r_id in d_pr[pro][:5]:
                 link_str += """<a href=\"/review?r_id="""+str(r_id)+"""\" target=\"_blank\">"""+str(r_id)+""",</a>"""
             t_rows.append(Item(title, developer, tags, genres,link_str))
+
     table = ItemTable(t_rows)
     html = str(table.__html__())
     html = html.replace("&#34;","\"")
@@ -108,7 +148,12 @@ def search():
     html = html.replace("&gt;", ">")
     end = time.time()
     print(end - start)
-    return html
+    html_str = "pridict results : <br>-----------------<br>"
+    for i in range(0,len(pre_list)):
+        html_str += str(pre_list[i][1]) + " : " + str(pre_list[i][0]) + "<br>"
+    html_str += html
+    conn.close()
+    return html_str
 
 @app.route("/review",methods=['GET','POST'])
 def show_review():
@@ -119,7 +164,7 @@ def show_review():
     sql_q = "select text , product_id , username from reviews where r_id="+str(query)
     c.execute(sql_q)
     row = c.fetchone()
-    text , product_id , username  = row
+    r_text , product_id , username  = row
     sql_q = "select title,developer,specs,tags,release_date from products where id="+str(product_id)
     c.execute(sql_q)
     row = c.fetchone()
@@ -127,7 +172,7 @@ def show_review():
     result = "Review Number : " + str(query)+"<br><br>"
     result += "Review"+"<br>"
     result += "User Name : " + username + "<br>"
-    result += "Review Context"+"<br>"+text+"<br><br>"
+    result += "Review Context"+"<br>"+r_text+"<br><br>"
 
     result += str("-"*10) +"<br>"
     result += "Title : " + str(title) +"<br>"
@@ -135,7 +180,47 @@ def show_review():
     result += "Specs : " + str(specs)+"<br>"
     result += "Tags : " + str(tags)+"<br>"
     result += "Release Date : " + str(release_date)+"<br>"
-    return result
+    conn2 = sqlite3.connect("game_data_modi.db")
+    sql_q = """select product_id , text from reviews where reviews.hours > 150 and length(text) > 100 and reviews.found_funny > 3 not like "0" AND product_id not like "%None%" """
+    c = conn2.cursor()
+    c.execute(sql_q)
+    rows = c.fetchall()
+    product_ids = []
+    texts = []
+    for product_id , text in rows:
+        product_ids.append(product_id)
+        texts.append(text)
+
+    X = app.count_vect.transform(texts)
+    X_tfidf = app.tfidf_transformer.transform(X)
+
+    r_X = app.count_vect.transform([r_text])
+    r_X_tfidf = app.tfidf_transformer.transform(r_X)
+
+    sim = Similarity()
+    result_siml = sim.calculate(X_tfidf,r_X_tfidf)
+    ordered = sorted(range(len(result_siml)), key=lambda k: result_siml[k], reverse=True)
+    products_list = []
+    counter = 0
+    for i in ordered:
+        if product_ids[i] not in products_list and counter < 10:
+            products_list.append(product_ids[i]) # list of products.
+            counter += 1
+
+    t_rows = []
+    for pro in products_list:
+        c.execute("select title,developer,genres,tags from products WHERE id=" + str(pro))
+        rows = c.fetchall()
+        for title, developer, tags, genres in rows:
+            t_rows.append(r_Item(title, developer, tags, genres))
+
+    table = RecommendedItemTable(t_rows)
+    html = "<br>"+"----------Recommended----------"+"<br>"+str(table.__html__())
+    html = html.replace("&#34;", "\"")
+    html = html.replace("&lt;", "<")
+    html = html.replace("&gt;", ">")
+
+    return result + " <br> "+html
 
 
 
@@ -147,6 +232,12 @@ class ItemTable(Table):
     tags = Col("Tags")
     links = Col("reviews")
 
+class RecommendedItemTable(Table):
+    title = Col("Title")
+    developer = Col("Developer")
+    genres = Col("Genres")
+    tags = Col("Tags")
+
 class Item(object):
     def __init__(self, title,developer,genres,tags,reviews):
         self.title = title
@@ -155,11 +246,20 @@ class Item(object):
         self.genres = genres
         self.links = reviews
 
-if __name__ == '__main__':
+class r_Item(object):
+    def __init__(self, title,developer,genres,tags):
+        self.title = title
+        self.developer = developer
+        self.tags = tags
+        self.genres = genres
 
-    app.dictionary = gensim.corpora.Dictionary.load("./dictionary.gensim")
-    app.data = None
-    print(app.dictionary)
-    with open('dictionary.pickle', 'rb') as handle:
-        app.data = pickle.load(handle)
-    app.run(host='0.0.0.0',debug=True)
+if __name__ == '__main__':
+    if not hasattr(app,"count_vect"):
+        with open('MyCountVectorizer.pickle', 'rb') as handle:
+            app.count_vect = pickle.load(handle)
+        with open('MyTfIdfVectorizer.pickle', 'rb') as f:
+            app.tfidf_transformer = pickle.load(f)
+        with open('MyNaiveBayes.pickle', 'rb') as NB:
+            app.clf = pickle.load(NB)
+    app.run(host='0.0.0.0', debug=True)
+
